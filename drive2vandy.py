d6a61e62d6059ac7d9c2b20d07c059c18db140f0
chengaVU <alex.cheng@vanderbilt.edu>
2015-10-26 15:09:29 -0500

    commented and cleaned up

--- drive2vandy.py
+++ drive2vandy.py
@@ -1,26 +1,30 @@
-# -*- coding: utf-8 -*-
+'''
+Script takes a list of addresses and calculates the driving distance and duration of those addresses
+to a destination. Afterwards, it calculates the corresponding zipcodes to that
+destination
+'''
 
 import json
 import urllib2
 import csv
+import time
 
-apiKey = 'AIzaSyCU2GxBn2C4KnLX3tGOvw2NBkFTAXStkIA'
+apiKey = 'AIzaSyCU2GxBn2C4KnLX3tGOvw2NBkFTAXStkIA' #Alex's API can get 2500 elements/day, 100 elements/query, 100 elements/10 seconds
 
 addresses = []
 zipcodes = []
 f = open('nashville_hc_addresses_only.txt','U')
 for line in f:
     line = line.strip()
-    line = line.replace('#','') ### URL request does not take # sign
     addresses.append(line)
     if line[-5] == '-':  ### extract 5 digit zip from 9 digits
         zipcodes.append(line[-10:-5])
     else:
         zipcodes.append(line[-5:])
         
-destination = '719 Thompson Lane, Nashville, TN 37204' ### 100 oaks addres
+destination = '719 Thompson Lane, Nashville, TN 37204' ### 100 oaks address
 mode = 'driving' ### can also take 'transit' or 'walking'
-units = 'metric' ### Value of distance is always in meters! Time is in seconds
+units = 'imperial' ### Value of distance is always in meters! Time is in seconds
 
 limit = 40  # limit of addresses per query determined by trial and error (may be character based)
 
@@ -28,7 +32,6 @@ with open('drive2vandy.csv', 'wb') as f:
     writer = csv.writer(f)
     rowcount=0
     addresses = addresses + zipcodes
-    output = []
     for n in range(len(addresses)/limit+1):
         listStart = n*limit
         listEnd = (n+1)*limit
@@ -41,18 +44,20 @@ with open('drive2vandy.csv', 'wb') as f:
                     + '&mode=' + mode \
                     + '&units=' + units \
                     + '&key=' + apiKey
-        request = request.replace(" ", "_")
+        request = request.replace(" ", "_") ### URL request does not take # sign or spaces
+        request= request.replace('#','') 
         data = json.load(urllib2.urlopen(request))
-        print data
+        time.sleep(1) # need a small time delay not to exceed rate-limit
         i = 0
         for element in data['rows']:
             status = element['elements'][0]['status']
             if status != 'OK':
-                row = [addresses[i],status,status]
+                row = [addresses[i],status,status] #usually means address not found, use original address
             else:
-                row = [data['origin_addresses'][i],element['elements'][0]['distance']['value'],element['elements'][0]['duration']['value']]
-            output.append(row)
-            print output
+                originAddress = data['origin_addresses'][i]
+                drivingDistance = element['elements'][0]['distance']['value']
+                drivingDuration = element['elements'][0]['duration']['value']
+                row = [originAddress,drivingDistance,drivingDuration]
             writer.writerow(row) 
             rowcount+=1
             i += 1
